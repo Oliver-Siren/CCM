8bb8c05b00a45a2ed18f5e4ba302698c2324d9f7
Eero Kolkki <eero.kolkki@myy.haaga-helia.fi>
2017-09-22 18:44:52 +0300

    Muokattu moduuleita

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -1,12 +1,5 @@
 class { "lampstack":}
-        Lampstack::Vhost {
-            notify => Service["apache2"],
-            }
-            lampstack::vhost {"eerokolkki":}
-            lampstack::vhost {"pulla":}
-
-        class { '::mysql::server':
-  root_password           => 'K4l4kukk0',
-  remove_default_accounts => true,
-  override_options        => $override_options
-}
+class { "mysql::server":
+            root_password => "K4l4kukk0",
+        }
+class { "tausta":}
