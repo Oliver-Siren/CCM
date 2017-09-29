cdc977323bf5a992b83c06874c31912b64de4501
jk-github <jarkko.koski@myy.haaga-helia.fi>
2017-09-29 19:30:09 +0300

    Did more stuff for chef tutorial created lamp_role problem description in extras

--- chef/Chef/chef-repo/cookbooks/learn_chef_apache2/recipes/default.rb
+++ chef/Chef/chef-repo/cookbooks/learn_chef_apache2/recipes/default.rb
@@ -14,7 +14,17 @@ service 'apache2' do
   supports status: true
   action [:enable, :start]
 end
+group 'web_admin'
+
+user 'web_admin' do
+  group 'web_admin'
+  system true
+  shell '/bin/bash'
+end
 
 template '/var/www/html/index.html' do # ~FC033
   source 'index.html.erb'
+  mode '0644'
+  owner 'web_admin'
+  group 'web_admin'
 end
