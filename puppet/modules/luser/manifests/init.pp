4839753465dce1b57f9eb00189435f79f1065d64
eerokolkki <eero.kolkki@myy.haaga-helia.fi>
2017-10-11 17:55:18 +0300

    Update init.pp

--- puppet/modules/luser/manifests/init.pp
+++ puppet/modules/luser/manifests/init.pp
@@ -1,11 +1,9 @@
 class luser {
 user { 'opiskelija':
-      ensure  => 'present',
-      comment => 'Opiskelija,,,',
+      ensure  => present,
       password => '$1$SomeSalt$4.JJVvoUTkZesyvyvpkll1',
-      gid => '1001',
       home => '/home/opiskelija',
       shell => '/bin/bash',
-      uid => '1001',
+      managehome => true,
     }
 }
