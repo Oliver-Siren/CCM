801121a05db8e146549d5b6cc407b7e1832fb7b2
eerokolkki <eero.kolkki@myy.haaga-helia.fi>
2017-10-11 16:34:37 +0300

    Update init.pp

--- puppet/modules/luser/manifests/init.pp
+++ puppet/modules/luser/manifests/init.pp
@@ -1,8 +1,11 @@
 class luser {
 user { 'opiskelija':
       ensure  => 'present',
-      comment => 'Opiskelija käyttäjätili,,,',
+      comment => 'Opiskelija,,,',
       password => '$1$SomeSalt$4.JJVvoUTkZesyvyvpkll1',
-      shell   => '/bin/bash',
+      gid => '1001',
+      home => '/home/opiskelija',
+      shell => '/bin/bash',
+      uid => '1001',
     }
 }
