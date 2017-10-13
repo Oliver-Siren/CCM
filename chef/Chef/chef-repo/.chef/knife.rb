34774d7e6217de3cebcb7597fe01266cfc5994fc
jarkko <jarkko.koski@myy.haaga-helia.fi>
2017-10-13 18:33:50 +0300

    finished chefDK and chefserver install guide started chefnode guide

--- chef/Chef/chef-repo/.chef/knife.rb
+++ chef/Chef/chef-repo/.chef/knife.rb
@@ -1,7 +1,7 @@
 current_dir = File.dirname(__FILE__)
 log_level                 :info
 log_location              STDOUT
-node_name                 "chefadmin"
+node_name                 "pingviinilaeppa"
 client_key                "#{current_dir}/chefadmin.pem"
-chef_server_url           "https://192.168.1.36/organizations/4thcoffee"
+chef_server_url           "https://vagrant.vm/organizations/arctic"
 cookbook_path             ["#{current_dir}/../cookbooks"]
